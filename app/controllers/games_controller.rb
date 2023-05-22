a52538842d6a5e55f351c0370c4e0d86a8b0e96c
Nui Suriya <atchara.suriya@gmail.com>
2023-05-23 01:12:57 +0200

    Created new's view&& score's view

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U)
+
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+  end
+
+end
